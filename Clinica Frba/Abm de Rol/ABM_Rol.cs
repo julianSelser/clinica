443457f78b40dbda73b73cc8238fb7f62f854e35
hernanm92 <herno_92@hotmail.com>
2013-10-30 17:09:36 -0300

    vista abm de rol

--- Clinica Frba/Abm de Rol/ABM_Rol.cs
+++ Clinica Frba/Abm de Rol/ABM_Rol.cs
@@ -6,14 +6,38 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Clinica_Frba.AppModel;
 
 namespace Clinica_Frba.Abm_de_Rol
 {
     public partial class ABM_Rol : Form
     {
-        public ABM_Rol()
+        private Form padre;
+
+        public ABM_Rol(Form padre)
         {
             InitializeComponent();
+            this.padre = padre;
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            AsistenteVistas.volverAPadreYCerrar(padre, this);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //AsistenteVistas.mostrarNuevaVentana(new Alta_Rol(this), this);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //AsistenteVistas.mostrarNuevaVentana(new Baja_Rol(this), this);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //AsistenteVistas.mostrarNuevaVentana(new Modificar_Rol(this), this);
         }
     }
 }
