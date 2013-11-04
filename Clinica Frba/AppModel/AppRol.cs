fd22e59a9f1e6a97dd06b21d2071a63b2b3c07a8
hernanm92 <herno_92@hotmail.com>
2013-11-04 16:27:26 -0300

    logica en la aplicacion de roles

--- Clinica Frba/AppModel/AppRol.cs
+++ Clinica Frba/AppModel/AppRol.cs
@@ -19,5 +19,45 @@ namespace Clinica_Frba.AppModel
         {
             ejecutarProcedure("inhabilitarRol", idRol);
         }
+
+        private static void modificarRol1(int idRol) //carga una vista con los datos apartir de un id, en este caso el nombre y las funcionalidades
+        {
+            List<String> listaDeFuncionalidades;
+
+            listaDeFuncionalidades = getFuncionalidadesRol(idRol);
+
+            DataTable tablaNombre = traerDataTable("getNombreRol", idRol);
+            DataRowCollection rowNombre = tablaNombre.Rows;
+
+            foreach (DataRow dr in rowNombre) //lo hago por mas que la lista tenga un solo valor
+            {
+                String nombre = dr["NombreRol"].ToString();
+            }
+        }
+
+        private static void modificarRol2(string nombre) //modifica los datos
+        {
+            //va a setear el nuevo nombre
+            //va a hacer un delet de todas las anteriores funcionalidades de ese id
+            //va a hacer un insert de todas las nuevas funcionalidades
+        }
+
+        private static List<String> getFuncionalidadesRol(int idRol) //modifica los datos
+        {
+            List<String> nombresFuncionalidades = new List<String>();
+
+            DataTable listaF = traerDataTable("getFuncionalidadesRol", idRol);
+
+            DataRowCollection rowFuncionalidad = listaF.Rows; //hace una lista de filas atravez de la tabla
+
+            foreach (DataRow dr in rowFuncionalidad) //para cada elemento de la lista
+            {
+                String nombre = dr["Descripcion"].ToString(); //seteo el cambo descripcion de esa fila
+                nombresFuncionalidades.Add(nombre);
+            }
+            //hasta aca mete los nombres de la tabla de funcionalidades en una lista
+
+            return nombresFuncionalidades;
+        }
     }
 }
