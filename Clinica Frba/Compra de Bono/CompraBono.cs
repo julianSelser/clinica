4cd5e1830737877cc8b858bbaf7fb30014b6cb4a
Dario Cappellini <dcappellini@hotmail.com.ar>
2013-11-10 22:26:53 -0300

    Compra de bonos completa (creo)

--- Clinica Frba/Compra de Bono/CompraBono.cs
+++ Clinica Frba/Compra de Bono/CompraBono.cs
@@ -25,6 +25,100 @@ namespace Clinica_Frba.Compra_de_Bono
             AsistenteVistas.volverAPadreYCerrar(padre, this);
         }
 
+        private void botonComprar_Click(object sender, EventArgs e)
+        {
+            int cant_consulta;
+            int cant_farmacia;
+            decimal nro_afiliado;
+            int monto_total;
+
+            if (labNroAfiliado.Visible == false)
+            {
+                MessageBox.Show("Debe seleccionar un afiliado.", "Error");
+                return;
+            }
+            
+            nro_afiliado = Convert.ToDecimal(labNroAfiliado.Text);
+
+            try
+            {
+                cant_consulta = Convert.ToInt16(textCantBonosConsulta.Text);
+                cant_farmacia = Convert.ToInt16(textCantBonosFarmacia.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Por favor ingrese cantidades válidas de bonos", "Error");
+                return;
+            }
+
+            monto_total = ConectorSQL.ejecutarProcedureWithReturnValue("comprarBonos", nro_afiliado, cant_farmacia, cant_consulta);
+
+            MessageBox.Show("Se realizó la compra solicitada. Importe total: $" + monto_total.ToString() + ".", "Éxito");
+        }
+
+        private void botonQuitarAfiliado_Click(object sender, EventArgs e)
+        {
+            labNroPlan.Visible = false;
+            labPrecioConsulta.Visible = false;
+            labPrecioFarmacia.Visible = false;
+
+            botonBuscarAfiliado.Visible = true;
+            botonQuitarAfiliado.Visible = false;
+
+            labNroAfiliado.Visible = false;
+            textBox1.Visible = true;
+        }
+
+        private void botonBuscarAfiliado_Click(object sender, EventArgs e)
+        {
+            decimal num_afiliado;
+            DataTable datos_compra;
+
+            if (textBox1.Text.Length > 18)
+            {
+                MessageBox.Show("Debe ingresar un número de afiliado válido.", "Error");
+                return;
+            }
+
+            try
+            {
+                num_afiliado = Convert.ToDecimal(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Debe ingresar un número de afiliado válido.", "Error");
+                return;
+            }
+
+
+            datos_compra = ConectorSQL.traerDataTable("getDatosParaCompra", num_afiliado);
+
+            if (datos_compra.Rows.Count.Equals(0))
+            {
+                MessageBox.Show("No se encontró al socio.", "Error");
+                return;
+            }
+
+            labNroPlan.Text = datos_compra.Rows[0]["Descripcion"].ToString();
+            labPrecioConsulta.Text = datos_compra.Rows[0]["Precio_Bono_Consulta"].ToString();
+            labPrecioFarmacia.Text = datos_compra.Rows[0]["Precio_Bono_Farmacia"].ToString();
+
+            labNroPlan.Visible = true;
+            labPrecioConsulta.Visible = true;
+            labPrecioFarmacia.Visible = true;
+
+            botonBuscarAfiliado.Visible = false;
+            botonQuitarAfiliado.Visible = true;
+
+            labNroAfiliado.Text = textBox1.Text;
+            labNroAfiliado.Visible = true;
+            textBox1.Visible = false;
+
+            
+            
+
+        }
+
 
     }
 }
