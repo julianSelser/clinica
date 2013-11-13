a2efc39375ede54ca567bbc81348900806ec1d18
julian <julianSelser@yahoo.com.ar>
2013-11-12 21:18:15 -0300

    termino codigo de login y ventanas

---
+++ Clinica Frba/Login/ElegirRol.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Clinica_Frba.AppModel;
+using Clinica_Frba.Domain;
+
+namespace Clinica_Frba.Login
+{
+    public partial class ElegirRol : Form
+    {
+        PantallaPrincipal pantallaPrincipal;
+        bool acepto = false;
+
+        public ElegirRol(PantallaPrincipal padre)
+        {
+            InitializeComponent();
+            this.pantallaPrincipal = padre;
+
+            rolComboBox.DataSource = AppLogin.traerRoles();
+            rolComboBox.DisplayMember = "nombre";
+        }
+
+        private void ElegirRol_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+            acepto = true;
+
+            UsuarioLogeado.Instance.Rol = (Rol) rolComboBox.SelectedItem;
+
+            var a = UsuarioLogeado.Instance.Rol.funcionalidades = AppLogin.traerFuncionalidades();
+
+            AppLogin.cargarUsuario();
+
+            if (UsuarioLogeado.Instance.Rol.nombre == "Administrativo")
+                pantallaPrincipal.setearLogeo("Administrador");
+            else
+                pantallaPrincipal.setearLogeo(UsuarioLogeado.Instance.Persona.nombre.ToUpper() + " " +UsuarioLogeado.Instance.Persona.apellido.ToUpper());
+
+            pantallaPrincipal.habilitarFuncionalidades();
+
+            AsistenteVistas.volverAPadreYCerrar(pantallaPrincipal, this);
+        }
+
+        private void ElegirRol_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = (acepto != true)? true : false;
+        }
+    }
+}
--- Clinica Frba/Login/LoginForm.cs
+++ Clinica Frba/Login/LoginForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Clinica_Frba.AppModel;
+using Clinica_Frba.Domain;
 
 namespace Clinica_Frba.Login
 {
@@ -19,7 +20,6 @@ namespace Clinica_Frba.Login
             pantallaPrincipal = principal;
             InitializeComponent();
             InitializePassword();
-
         }
 
         private void InitializePassword()
@@ -31,8 +31,26 @@ namespace Clinica_Frba.Login
 
         private void acceptButton_Click(object sender, EventArgs e)
         {
-            pantallaPrincipal.setearUsuarioLogueado(usrNameTextBox.Text);
-            AsistenteVistas.volverAPadreYCerrar(pantallaPrincipal, this);
+            int n_intentos = AppLogin.intentarLogear(usrNameTextBox.Text, passTextBox.Text);
+            
+            if (0 == n_intentos) 
+            {
+                UsuarioLogeado.Instance.UserName = usrNameTextBox.Text;
+
+                AsistenteVistas.mostrarNuevaVentana(new ElegirRol(pantallaPrincipal), this);
+            }
+            else
+	        {
+                if(n_intentos < 3) 
+                    MessageBox.Show("Contraseña erronea. Le quedan " + ( 3 - n_intentos ) + " intentos." );
+                else if(n_intentos == 3)
+                    MessageBox.Show("Contraseña erronea. Se procederá a deshabilitar su usuario por seguridad." );
+                else if (n_intentos == 4)
+                    MessageBox.Show("Su usuario se encuentra deshabilitado por razones de seguridad. " +
+                                    "Consulte con su administrador para volver a habilitarlo.");
+                else if(n_intentos == 5)
+                    MessageBox.Show("No existe el usuario ingresado en el sistema");
+	        }
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
