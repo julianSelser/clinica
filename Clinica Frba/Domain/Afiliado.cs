411100c743f91d2883e15bccf811dcf7455d8ff7
julian <julianSelser@yahoo.com.ar>
2013-11-12 22:16:19 -0300

    modifico el dominio para tener usuario global

--- Clinica Frba/Domain/Afiliado.cs
+++ Clinica Frba/Domain/Afiliado.cs
@@ -8,18 +8,9 @@ using System.Text;
  */
 namespace Clinica_Frba.Domain
 {
-    class Afiliado
+    class Afiliado : Persona
     {
         public int nroAfiliado { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public char sexo { get; set; }
-        public string tipoDoc { get; set; }
-        public int nroDoc { get; set; }
-        public string direccion { get; set; }
-        public string mail { get; set; }
-        public int telefono { get; set; }
-        public DateTime fechaNac { get; set; }
         public int codPlan { get; set; }
         public string estadoCivil { get; set; }
 	    public int nroTitular { get; set; }
@@ -28,8 +19,29 @@ namespace Clinica_Frba.Domain
 	    public int nroConsulta { get; set; }
         public int cantFamiliaresACargo { get; set; }
 
-        public Afiliado()
+        public Afiliado(int _nroAfiliado, string _nombre, string _apellido, char _sexo, string _tipoDoc, int _nroDoc, string _direccion,
+            string _mail, int _telefono, DateTime _fechaNac, int _codPlan, string _estadoCivil, int _nroTitular, int _nroConyuge, 
+            DateTime _fechaBaja, int _nroConsulta, int _cantFamiliaresACargo)
         {
+            this.nroAfiliado = _nroAfiliado;
+            this.nombre = _nombre;
+            this.apellido = _apellido;
+            this.sexo = _sexo;
+            this.tipoDoc = _tipoDoc;
+            this.nroDoc = _nroDoc;
+            this.direccion = _direccion;
+            this.telefono = _telefono;
+            this.mail = _mail;
+            this.fechaNac = _fechaNac;
+            this.codPlan = _codPlan;
+            this.estadoCivil = _estadoCivil;
+            this.nroTitular = _nroTitular;
+            this.nroConyuge = _nroConyuge;
+            this.fechaBaja = _fechaBaja;
+            this.nroConsulta = _nroConsulta;
+            this.cantFamiliaresACargo = _cantFamiliaresACargo;
         }
+
+        public Afiliado() { }
     }
 }
--- Clinica Frba/Domain/Profesional.cs
+++ Clinica Frba/Domain/Profesional.cs
@@ -5,21 +5,32 @@ using System.Text;
 
 namespace Clinica_Frba.Domain
 {
-    class Profesional
+    class Profesional : Persona
     {
-        public int id { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public char sexo { get; set; }
-        public string tipoDoc { get; set; }
-        public int nroDoc { get; set; }
-        public string direccion { get; set; }
-        public int telefono { get; set; }
-        public string mail { get; set; }
-        public DateTime fechaNac { get; set; }
         public int nroMatricula { get; set; }
         public DateTime fechaAtencionHasta { get; set; }
         public DateTime fechaAtencionDesde { get; set; }
         public bool habilitado { get; set; }
+        public int id { get; set; }
+
+        public Profesional(int _id, string _nombre, string _apellido, char _sexo, string _tipoDoc, int _nroDoc, string _direccion, int _telefono, string _mail, DateTime _fechaNac, int _nroMatricula, DateTime _fechaAtencionDesde, DateTime _fechaAtencionHasta, bool _habilitado)
+        {
+            this.id = _id;
+            this.nombre = _nombre;
+            this.apellido= _apellido;
+            this.sexo = _sexo;
+            this.tipoDoc = _tipoDoc;
+            this.nroDoc= _nroDoc;
+            this.direccion = _direccion;
+            this.telefono = _telefono;
+            this.mail = _mail;
+            this.fechaNac = _fechaNac;
+            this.nroMatricula = _nroMatricula;
+            this.fechaAtencionHasta = _fechaAtencionHasta;
+            this.fechaAtencionDesde = _fechaAtencionDesde;
+            this.habilitado = _habilitado;
+        }
+
+        public Profesional() { }
     }
 }
