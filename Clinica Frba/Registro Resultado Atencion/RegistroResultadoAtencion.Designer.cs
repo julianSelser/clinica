6e2174fdab3808a5d2536398cbc79d5e6ef89aaf
Jonathan Coralo <jonathan.corallo92@gmail.com>
2013-11-19 14:13:56 -0300

    Registrar Resultado Atencion
    -Layout de la pantalla de busqueda principal de registrado resultado
    -Asignacion del ID de medico al textbox desde el Singlenton de usuario

--- Clinica Frba/Registro Resultado Atencion/RegistroResultadoAtencion.Designer.cs
+++ Clinica Frba/Registro Resultado Atencion/RegistroResultadoAtencion.Designer.cs
@@ -28,51 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.fechaAtencionPicker = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nroAfiliadoBox = new System.Windows.Forms.TextBox();
+            this.selectAfiliadoButton = new System.Windows.Forms.Button();
+            this.selectProfesionalButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.profesionalBox = new System.Windows.Forms.TextBox();
+            this.errorBox = new System.Windows.Forms.TextBox();
+            this.limpiarButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button2
+            // cancelButton
             // 
-            this.button2.Location = new System.Drawing.Point(164, 199);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 11;
-            this.button2.Text = "Cancelar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(43, 199);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Aceptar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.cancelButton.Location = new System.Drawing.Point(652, 400);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 11;
+            this.cancelButton.Text = "Cancelar";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(39, 82);
+            this.label1.Location = new System.Drawing.Point(448, 91);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(193, 24);
             this.label1.TabIndex = 12;
             this.label1.Text = "EN CONSTRUCCION";
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 181);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(715, 202);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // fechaAtencionPicker
+            // 
+            this.fechaAtencionPicker.Location = new System.Drawing.Point(121, 122);
+            this.fechaAtencionPicker.Name = "fechaAtencionPicker";
+            this.fechaAtencionPicker.Size = new System.Drawing.Size(200, 20);
+            this.fechaAtencionPicker.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.selectProfesionalButton);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.profesionalBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.nroAfiliadoBox);
+            this.groupBox1.Controls.Add(this.selectAfiliadoButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.fechaAtencionPicker);
+            this.groupBox1.Location = new System.Drawing.Point(25, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(338, 162);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos de Consulta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Fecha de Atención:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Afiliado:";
+            // 
+            // nroAfiliadoBox
+            // 
+            this.nroAfiliadoBox.Location = new System.Drawing.Point(73, 34);
+            this.nroAfiliadoBox.Name = "nroAfiliadoBox";
+            this.nroAfiliadoBox.ReadOnly = true;
+            this.nroAfiliadoBox.Size = new System.Drawing.Size(100, 20);
+            this.nroAfiliadoBox.TabIndex = 22;
+            // 
+            // selectAfiliadoButton
+            // 
+            this.selectAfiliadoButton.Location = new System.Drawing.Point(190, 32);
+            this.selectAfiliadoButton.Name = "selectAfiliadoButton";
+            this.selectAfiliadoButton.Size = new System.Drawing.Size(131, 23);
+            this.selectAfiliadoButton.TabIndex = 24;
+            this.selectAfiliadoButton.Text = "Seleccionar Afiliado";
+            this.selectAfiliadoButton.UseVisualStyleBackColor = true;
+            // 
+            // selectProfesionalButton
+            // 
+            this.selectProfesionalButton.Location = new System.Drawing.Point(190, 75);
+            this.selectProfesionalButton.Name = "selectProfesionalButton";
+            this.selectProfesionalButton.Size = new System.Drawing.Size(131, 23);
+            this.selectProfesionalButton.TabIndex = 27;
+            this.selectProfesionalButton.Text = "Seleccionar profesional";
+            this.selectProfesionalButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Profesional:";
+            // 
+            // profesionalBox
+            // 
+            this.profesionalBox.Location = new System.Drawing.Point(73, 78);
+            this.profesionalBox.Name = "profesionalBox";
+            this.profesionalBox.ReadOnly = true;
+            this.profesionalBox.Size = new System.Drawing.Size(100, 20);
+            this.profesionalBox.TabIndex = 26;
+            // 
+            // errorBox
+            // 
+            this.errorBox.Location = new System.Drawing.Point(393, 24);
+            this.errorBox.Multiline = true;
+            this.errorBox.Name = "errorBox";
+            this.errorBox.ReadOnly = true;
+            this.errorBox.Size = new System.Drawing.Size(334, 39);
+            this.errorBox.TabIndex = 25;
+            // 
+            // limpiarButton
+            // 
+            this.limpiarButton.Location = new System.Drawing.Point(652, 136);
+            this.limpiarButton.Name = "limpiarButton";
+            this.limpiarButton.Size = new System.Drawing.Size(75, 23);
+            this.limpiarButton.TabIndex = 27;
+            this.limpiarButton.Text = "Limpiar";
+            this.limpiarButton.UseVisualStyleBackColor = true;
+            // 
             // RegistroResultadoAtencion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(739, 435);
+            this.Controls.Add(this.limpiarButton);
+            this.Controls.Add(this.errorBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cancelButton);
             this.Name = "RegistroResultadoAtencion";
             this.Text = "Registro Resultado de Atención";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,8 +201,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker fechaAtencionPicker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nroAfiliadoBox;
+        private System.Windows.Forms.Button selectAfiliadoButton;
+        private System.Windows.Forms.Button selectProfesionalButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox profesionalBox;
+        private System.Windows.Forms.TextBox errorBox;
+        private System.Windows.Forms.Button limpiarButton;
     }
 }
--- Clinica Frba/Registro Resultado Atencion/RegistroResultadoAtencion.cs
+++ Clinica Frba/Registro Resultado Atencion/RegistroResultadoAtencion.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Clinica_Frba.AppModel;
+using Clinica_Frba.Domain;
 
 namespace Clinica_Frba.Registro_Resultado_Atencion
 {
@@ -18,11 +19,16 @@ namespace Clinica_Frba.Registro_Resultado_Atencion
         {
             InitializeComponent();
             this.padre = padre;
+            if (UsuarioLogeado.Instance.Rol.nombre == "Profesional")
+            {
+                profesionalBox.Text = (UsuarioLogeado.Instance.Persona as Profesional).id.ToString();
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void cancelButton_Click(object sender, EventArgs e)
         {
             AsistenteVistas.volverAPadreYCerrar(padre, this);
         }
+
     }
 }
