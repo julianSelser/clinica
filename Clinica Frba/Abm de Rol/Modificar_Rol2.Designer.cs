098816013fd8e30c50abfd9f04792cd633ca55dc
hernanm92 <herno_92@hotmail.com>
2013-11-20 16:07:50 -0300

    modifico ventana para nuevas funcionalidades

--- Clinica Frba/Abm de Rol/Modificar_Rol2.Designer.cs
+++ Clinica Frba/Abm de Rol/Modificar_Rol2.Designer.cs
@@ -34,11 +34,15 @@
             this.chkBoxFuncionalidades = new System.Windows.Forms.CheckedListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.idBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.errorBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(165, 223);
+            this.button2.Location = new System.Drawing.Point(166, 324);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 11;
@@ -48,7 +52,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(54, 221);
+            this.button1.Location = new System.Drawing.Point(54, 324);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 10;
@@ -59,7 +63,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(51, 72);
+            this.label2.Location = new System.Drawing.Point(51, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 13);
             this.label2.TabIndex = 9;
@@ -68,7 +72,7 @@
             // chkBoxFuncionalidades
             // 
             this.chkBoxFuncionalidades.FormattingEnabled = true;
-            this.chkBoxFuncionalidades.Location = new System.Drawing.Point(54, 88);
+            this.chkBoxFuncionalidades.Location = new System.Drawing.Point(54, 163);
             this.chkBoxFuncionalidades.Name = "chkBoxFuncionalidades";
             this.chkBoxFuncionalidades.Size = new System.Drawing.Size(187, 94);
             this.chkBoxFuncionalidades.TabIndex = 8;
@@ -76,7 +80,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(51, 23);
+            this.label1.Location = new System.Drawing.Point(54, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 6;
@@ -84,17 +88,55 @@
             // 
             // idBox
             // 
-            this.idBox.Location = new System.Drawing.Point(97, 20);
+            this.idBox.Location = new System.Drawing.Point(107, 77);
             this.idBox.Name = "idBox";
             this.idBox.ReadOnly = true;
             this.idBox.Size = new System.Drawing.Size(67, 20);
             this.idBox.TabIndex = 35;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(107, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 37;
+            // 
+            // errorBox
+            // 
+            this.errorBox.Location = new System.Drawing.Point(54, 12);
+            this.errorBox.Multiline = true;
+            this.errorBox.Name = "errorBox";
+            this.errorBox.ReadOnly = true;
+            this.errorBox.Size = new System.Drawing.Size(187, 34);
+            this.errorBox.TabIndex = 38;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(54, 283);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 39;
+            this.button3.Text = "Habilitar Rol";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Modificar_Rol2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(292, 359);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.errorBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.idBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -116,5 +158,9 @@
         private System.Windows.Forms.CheckedListBox chkBoxFuncionalidades;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox errorBox;
+        private System.Windows.Forms.Button button3;
     }
 }
