936df27ff83ee98b87d23e50d47ce1bed438b1a5
Jonathan Coralo <jonathan.corallo92@gmail.com>
2013-11-27 00:46:16 -0300

    Pedir Turno
    Agregado layout general

---
+++ Clinica Frba/AppModel/AppPedirTurno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Clinica_Frba.Domain;
+
+namespace Clinica_Frba.AppModel
+{
+    class AppPedirTurno : ConectorSQL
+    {
+
+        internal static DataTable traerFechasAgenda(Profesional profesional)
+        {
+            return traerDataTable("getFechasDisponiblesAgenda", profesional.id);
+        }
+
+        internal static DataTable traerTimeslotsFecha(Profesional profesional, DateTime fechaAgenda)
+        {
+            return traerDataTable("getTimeslotsFecha", profesional.id, fechaAgenda);
+        }
+    }
+}
